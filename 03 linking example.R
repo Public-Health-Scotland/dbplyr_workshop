646526f4a6a3bd21511f223816b59b57c4d1731d
James McMahon <james.mcmahon@phs.scot>
2023-11-01 14:41:08 +0000

    Reorder examples and add comments

--- 03 linking example.R
+++ 03 linking example.R
@@ -1,29 +1,42 @@
 # Doing more complicated extracts
 
-# Set up a deaths extract
-deaths_query <- tbl(smra_conn, in_schema("ANALYSIS", "GRO_DEATHS_C")) |>
-  select(LINK_NO, DATE_OF_DEATH) |>
-  filter(DATE_OF_DEATH >= To_date("2023-06-01", "YYYY-MM-DD"))
+# Previewing the table / object
+# Use `colnames()` to just print the variable names
 
-# Previewing the object
+# Without dbplyr (just odbc)
+odbcPreviewObject(
+  smra_conn,
+  rowLimit = 10,
+  table = "ANALYSIS.GRO_DEATHS_C"
+) |>
+  as_tibble() 
 
-odbcPreviewObject(smra_conn, 10, table = "ANALYSIS.GRO_DEATHS_C") |>
-  as_tibble()
-
+# If we just run a dbplyr query it will give us a sample of the data
 tbl(
   smra_conn,
   dbplyr::in_schema("ANALYSIS", "GRO_DEATHS_C")
 )
 
-deaths_query
-# Use `colnames()` to just print the variable names
+# Use `colnames()` to just print a list of the columns
+tbl(
+  smra_conn,
+  dbplyr::in_schema("ANALYSIS", "GRO_DEATHS_C")
+) |>
+  colnames()
 
+# Set up a deaths extract
+deaths_query <- tbl(smra_conn, in_schema("ANALYSIS", "GRO_DEATHS_C")) |>
+  select(LINK_NO, DATE_OF_DEATH) |>
+  filter(DATE_OF_DEATH >= To_date("2023-06-01", "YYYY-MM-DD"))
+
+# See a sample of the data
+deaths_query
 
 # See what the SQL looks like
 deaths_query |>
   show_query()
 
-# Set up a simple SMR01 extract (same as in the previous example)
+# Set up a simple SMR01 extract (similar to the previous example)
 simple_smr01_query <- tbl(smra_conn, "SMR01_PI") |>
   select(
     LINK_NO,
@@ -83,3 +96,5 @@ smr01_lothian <- simple_smr01_query |>
   ) |>
   show_query() |>
   collect()
+
+
