b439b61211f9197a28b240f73be353e423add6d1
James McMahon <james.mcmahon@phs.scot>
2023-11-01 11:58:16 +0000

    Expand workshop examples
    Use `|>` and update dates.

--- 02 simple example.R
+++ 02 simple example.R
@@ -5,7 +5,7 @@ library(dbplyr) # Sometimes we might need a specific dbplyr function...
 sql <- paste(
   "SELECT LINK_NO, ADMISSION_DATE, DISCHARGE_DATE",
   "FROM ANALYSIS.SMR01_PI",
-  "WHERE DISCHARGE_DATE >= To_date('2023-06-01', 'YYYY-MM-DD')"
+  "WHERE DISCHARGE_DATE >= To_date('2023-08-01', 'YYYY-MM-DD')"
 )
 
 # Do the extract using the usual dbGetQuery (from odbc)
@@ -13,12 +13,12 @@ simple_1_extract <- dbGetQuery(smra_conn, sql)
 
 # Use dbplyr
 # Note we have to use upper case variable names
-simple_2_query <- tbl(smra_conn, "SMR01_PI") %>%
-  select(LINK_NO, ADMISSION_DATE, DISCHARGE_DATE) %>%
-  filter(DISCHARGE_DATE >= To_date("2019-06-01", "YYYY-MM-DD"))
+simple_2_query <- tbl(smra_conn, in_schema("ANALYSIS", "SMR01_PI")) |>
+  select(LINK_NO, ADMISSION_DATE, DISCHARGE_DATE) |>
+  filter(DISCHARGE_DATE >= To_date("2023-08-01", "YYYY-MM-DD"))
 
 # This will print the 'translated' SQL for us to see
-simple_2_query %>% show_query()
+simple_2_query |> show_query()
 
 # Up until this point we haven't actually got the data
 simple_2_extract <- collect(simple_2_query)
--- 03 linking example.R
+++ 03 linking example.R
@@ -1,36 +1,47 @@
 # Doing more complicated extracts
 
 # Set up a deaths extract
-# For some reason need to use in_schema()
-deaths <-
-  tbl(
-    smra_conn,
-    dbplyr::in_schema("ANALYSIS", "GRO_DEATHS_C")
-  ) %>%
-  select(LINK_NO, DATE_OF_DEATH) %>%
-  filter(DATE_OF_DEATH >= To_date("2019-06-01", "YYYY-MM-DD"))
+deaths_query <- tbl(smra_conn, in_schema("ANALYSIS", "GRO_DEATHS_C")) |>
+  select(LINK_NO, DATE_OF_DEATH) |>
+  filter(DATE_OF_DEATH >= To_date("2023-06-01", "YYYY-MM-DD"))
 
-# Use colnames to check variable names
-colnames(tbl(
+# Previewing the object
+
+odbcPreviewObject(smra_conn, 10, table ="ANALYSIS.GRO_DEATHS_C") |> 
+  as_tibble() 
+
+tbl(
   smra_conn,
   dbplyr::in_schema("ANALYSIS", "GRO_DEATHS_C")
-))
+)
+
+deaths_query
+# Use `colnames()` to just print the variable names
+
 
 # See what the SQL looks like
-deaths %>% show_query()
+deaths_query |>
+  show_query()
 
 # Set up a simple SMR01 extract (same as in the previous example)
-simple_smr01 <- tbl(smra_conn, "SMR01_PI") %>%
-  select(LINK_NO, ADMISSION_DATE, DISCHARGE_DATE) %>%
-  filter(DISCHARGE_DATE >= To_date("2019-06-01", "YYYY-MM-DD"))
+simple_smr01_query <- tbl(smra_conn, "SMR01_PI") |>
+  select(
+    LINK_NO,
+    ADMISSION_DATE,
+    DISCHARGE_DATE,
+    HBRES_KEYDATE,
+    AGE_IN_YEARS
+  ) |>
+  filter(DISCHARGE_DATE >= To_date("2023-07-01", "YYYY-MM-DD"))
 
 # Do a link using dplyr joins
 # Alternative is (getting) complicated SQL
 # Or extract separately and then join which is to be avoided!
-linked_1_query <- simple_smr01 %>% inner_join(deaths)
+linked_1_query <- simple_smr01_query |> 
+  inner_join(deaths_query, by = "LINK_NO")
 
 # We could probably write some slightly better SQL but not as easily!
-linked_1_query %>% show_query()
+linked_1_query |> show_query()
 
 # Do the extract
 linked_1_extract <- collect(linked_1_query)
@@ -38,22 +49,36 @@ linked_1_extract <- collect(linked_1_query)
 # The larger the extracts the faster this method is compared to joining separate
 # extracts
 
-# This is easily extended to multiple links, for example by including SMR04
-simple_smr04 <- tbl(smra_conn, "SMR04_PI") %>%
-  select(LINK_NO, ADMISSION_DATE, DISCHARGE_DATE) %>%
-  # Pick a recent date for the example
-  filter(DISCHARGE_DATE >= To_date("2020-05-01", "YYYY-MM-DD"))
+# We can keep adding more manipulation code and create a complicated SQL query 
+# using only dbplyr.
+library(lubridate)
+library(janitor)
 
-# Do a full join on the SMR01 and 04 data
-linked_2_extract <- simple_smr01 %>%
-  # Flag where the data has come from
-  mutate(source = "SMR01") %>%
-  full_join(
-    simple_smr04 %>%
-      mutate(source = "SMR04")
-  ) %>%
-  # Inner join the deaths so we're left with only people who have died
-  inner_join(deaths) %>%
-  # Until this point we don't actually have any data
-  # We only have translated SQL
+smr01_deaths <- simple_smr01_query |> 
+  # Lothian
+  filter(HBRES_KEYDATE == "S08000024") |> 
+  filter(AGE_IN_YEARS >= 18) |> 
+  mutate(dis_month = month(DISCHARGE_DATE)) |> 
+  inner_join(deaths_query, by = "LINK_NO") |> 
+  clean_names() |> 
+  show_query() |> 
   collect()
+
+# Even group and do aggregation
+smr01_lothian <- simple_smr01_query |> 
+  # Lothian
+  filter(HBRES_KEYDATE == "S08000024") |> 
+  filter(AGE_IN_YEARS >= 18) |> 
+  mutate(age_gpr = case_when(
+    between(AGE_IN_YEARS, 18, 64) ~ "18-64",
+    between(AGE_IN_YEARS, 64, 89) ~ "18-89",
+    AGE_IN_YEARS >= 90 ~ "90+"
+  )) |> 
+  mutate(dis_month = month(DISCHARGE_DATE)) |> 
+  group_by(age_gpr, dis_month) |>
+  summarise(admissions = n(),
+            patients = n_distinct(LINK_NO),
+            .groups = "drop") |>
+  show_query() |> 
+  collect()
+  
